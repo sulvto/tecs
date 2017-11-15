9f1a979b15bf34f071df0c5a3838fb3269924b92
sulvto <sulvto@gmail.com>
2017-11-15 23:59:05 +0800

    add Project8

---
+++ Project8/VMtranslator/CodeWriter.java
@@ -0,0 +1,327 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sulvto on 17-11-12.
+ */
+public class CodeWriter {
+    private List<String> output = new ArrayList<>();
+    private int jumpFlag = 0;
+    private File outputFile;
+
+    public void setFileName(String filename) {
+        outputFile = new File(filename);
+    }
+
+    public void writeArithmetic(String command) {
+        switch (command) {
+            case "add": add(); break;
+            case "sub": sub(); break;
+            case "neg": neg(); break;
+            case "and": and(); break;
+            case "or":  or();  break;
+            case "not": not(); break;
+            case "eq":  eq();  break;
+            case "lt":  lt();  break;
+            case "gt":  gt();  break;
+        }
+    }
+
+    private void neg() {
+        write("@SP");
+        write("A=M-1");
+        write("D=0");
+        write("M=D-M");
+    }
+
+    private void sub() {
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("M=M-D");
+    }
+
+    private void add() {
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("M=M+D");
+    }
+
+    private void and() {
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("M=M&D");
+    }
+
+    private void or() {
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("M=M|D");
+    }
+
+    public void not() {
+        write("@SP");
+        write("A=M-1");
+        write("M=!M");
+    }
+
+    private void eq() {
+        int flag = jumpFlag++;
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("D=M-D");
+        write("@FALSE" + flag);
+        write("D;JEQ");
+
+        write("@SP");
+        write("A=M-1");
+        write("M=0");
+        write("@CONTINUE" + flag);
+        write("0;JMP");
+
+        write("(FALSE" + flag + ")");
+        write("@SP");
+        write("A=M-1");
+        write("M=-1");
+        write("(CONTINUE" + flag + ")");
+    }
+
+
+    private void lt() {
+        int flag = jumpFlag++;
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("D=M-D");
+        write("@FALSE" + flag);
+        write("D;JLT");
+
+        write("@SP");
+        write("A=M-1");
+        write("M=0");
+        write("@CONTINUE" + flag);
+        write("0;JMP");
+
+        write("(FALSE" + flag + ")");
+        write("@SP");
+        write("A=M-1");
+        write("M=-1");
+        write("(CONTINUE" + flag + ")");
+    }
+
+    private void gt() {
+        int flag = jumpFlag++;
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("A=A-1");
+        write("D=M-D");
+        write("@FALSE" + flag);
+        write("D;JGT");
+
+        write("@SP");
+        write("A=M-1");
+        write("M=0");
+        write("@CONTINUE" + flag);
+        write("0;JMP");
+
+        write("(FALSE" + flag + ")");
+        write("@SP");
+        write("A=M-1");
+        write("M=-1");
+        write("(CONTINUE" + flag + ")");
+    }
+
+
+    public void writePush(String segment, int index) {
+
+        switch (segment) {
+            case "constant":
+                write("@" + index);
+                write("D=A");
+                pushD();
+                break;
+            case "argument":
+                write("@ARG");
+                write("D=M");
+                write("@" + index);
+                write("A=A+D");
+                write("D=M");
+                pushD();
+                break;
+            case "local":
+                write("@LCL");
+                write("D=M");
+                write("@" + index);
+                write("A=A+D");
+                write("D=M");
+                pushD();
+                break;
+            case "static":
+                write("@" + (16 + index));
+                write("D=M");
+                pushD();
+                break;
+            case "this":
+                write("@THIS");
+                write("D=M");
+                write("@" + index);
+                write("A=A+D");
+                write("D=M");
+                pushD();
+                break;
+            case "that":
+                write("@THAT");
+                write("D=M");
+                write("@" + index);
+                write("A=A+D");
+                write("D=M");
+                pushD();
+                break;
+            case "pointer":
+                write("@" + (index == 0 ? "THIS" : "THAT"));
+                write("D=M");
+                pushD();
+                break;
+            case "temp":
+                write("@" + (5 + index));
+                write("D=M");
+                pushD();
+                break;
+            default:
+                throw new Error("");
+        }
+    }
+
+    private void pushD() {
+        write("@SP");
+        write("A=M");
+        write("M=D");
+        write("@SP");
+        write("M=M+1");
+    }
+
+    private void popD() {
+        write("@R13");
+        write("M=D");
+        write("@SP");
+        write("AM=M-1");
+        write("D=M");
+        write("@R13");
+        write("A=M");
+        write("M=D");
+    }
+
+    public void writePop(String segment, int index) {
+        switch (segment) {
+            case "constant":
+                // TODO
+                break;
+            case "argument":
+                write("@ARG");
+                write("D=M");
+                write("@" + index);
+                write("D=A+D");
+                popD();
+                break;
+            case "local":
+                write("@LCL");
+                write("D=M");
+                write("@" + index);
+                write("D=A+D");
+                popD();
+                break;
+            case "static":
+                write("@" + (16 + index));
+                write("D=A");
+                popD();
+                break;
+            case "this":
+                write("@THIS");
+                write("D=M");
+                write("@" + index);
+                write("D=A+D");
+                popD();
+                break;
+            case "that":
+                write("@THAT");
+                write("D=M");
+                write("@" + index);
+                write("D=A+D");
+                popD();
+                break;
+            case "pointer":
+                write("@" + (index == 0 ? "THIS" : "THAT"));
+                write("D=A");
+                popD();
+                break;
+            case "temp":
+                write("@" + (5 + index));
+                write("D=A");
+                popD();
+                break;
+            default:
+                throw new Error("");
+        }
+    }
+
+    public void writeInit(){
+
+    }
+
+    public void writeLabel(String label){
+
+    }
+
+    public void writeGoto(String label){
+
+    }
+
+    public void writeIf(String label){
+
+    }
+
+    public void writeCall(String funName, int numArgs){
+
+    }
+
+    public void writeReturn(){
+
+    }
+
+    public void writeFunction(String funName, int numLocals){
+
+    }
+
+    private void write(String command) {
+        output.add(command);
+    }
+
+    public void close() {
+        if (outputFile != null) {
+            try {
+                FileWriter fileWriter = new FileWriter(outputFile);
+                fileWriter.write(output.stream().reduce((s, s2) -> s + "\n" + s2).get());
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ Project8/VMtranslator/Parser.java
@@ -0,0 +1,86 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by sulvto on 17-11-11.
+ */
+public class Parser {
+    private final List<String> input = new ArrayList<>();
+
+    private String command;
+
+
+    public void init(InputStream inputStream) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while (true) {
+                int read = inputStream.read();
+                if (read != -1) stringBuilder.append((char) read);
+                else break;
+            }
+        } catch (IOException e) {
+            throw new Error(e.getMessage());
+        }
+        input.clear();
+        input.addAll(Stream.of(stringBuilder.toString().split("\n"))
+
+                .map(s -> {
+                    int i = s.indexOf("//");
+                    if (i != -1) {
+                        return s.substring(0, i);
+                    }
+                    return s;
+                }).map(String::trim)
+                .filter(s -> s.length() > 0).collect(Collectors.toList()));
+    }
+
+    public boolean hasMoreCommands() {
+        return input.size() > 0;
+    }
+
+    public void advance() {
+        command = input.remove(0).toLowerCase();
+        System.out.println("advance:" + command);
+    }
+
+    public String command() {
+        return command;
+    }
+
+    public CommandType commandType() {
+        if (command.startsWith("return")) {
+            return CommandType.RETURN;
+        } else if (command.startsWith("call")) {
+            return CommandType.CALL;
+        } else if (command.startsWith("function")) {
+            return CommandType.FUNCTION;
+        } else if (command.startsWith("if-goto")) {
+            return CommandType.IF;
+        } else if (command.startsWith("goto")) {
+            return CommandType.GOTO;
+        } else if (command.startsWith("label")) {
+            return CommandType.LABEL;
+        } else if (command.startsWith("push")) {
+            return CommandType.PUSH;
+        } else if (command.startsWith("pop")) {
+            return CommandType.POP;
+        } else {
+            return CommandType.ARITHMETIC;
+        }
+    }
+
+    public String arg1() {
+        return command.split("\\s+")[1];
+    }
+
+    public int arg2() {
+        return Integer.valueOf(command.split("\\s+")[2]);
+    }
+
+    enum CommandType {ARITHMETIC, PUSH, POP, LABEL, GOTO, IF, FUNCTION, RETURN, CALL}
+
+}
